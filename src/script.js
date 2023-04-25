a5eb9a173ae60e7f1bb376935239abc111f4db93
Kmendoza23 <k.mendozacontent@gmail.com>
2023-04-24 21:58:29 -0600

    axios retrieves apiURL

--- src/script.js
+++ src/script.js
@@ -22,14 +22,9 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function getForecast(coordinates) {
-  console.log(coordinates);
-  let apiKey = "0efb4fc16a9ed98dc0b3aafd8491d6ad";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
-}
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data);
   let forecastElement = document.querySelector("#forecast")
   
   let days = ["Mon", "Tues", "Wed", "Thurs"];
@@ -54,6 +49,13 @@ function displayForecast() {
   forecastHTML= forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
   console.log(forecastHTML);
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "0efb4fc16a9ed98dc0b3aafd8491d6ad";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function showCurrentTemperature(response) {
