31e9ec247571a515dff5b65ca297d236c776bc70
Kmendoza23 <k.mendozacontent@gmail.com>
2023-04-24 21:53:05 -0600

    corrected capitalization

--- src/script.js
+++ src/script.js
@@ -26,7 +26,7 @@ function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "0efb4fc16a9ed98dc0b3aafd8491d6ad";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiURL);
+  console.log(apiUrl);
 }
 
 function displayForecast() {
