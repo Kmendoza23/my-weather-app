405ae1b7aa2ad5f954c32753f7c79add1d8b76c3
Kmendoza23 <k.mendozacontent@gmail.com>
2023-04-25 21:32:21 -0600

    Update script.js

--- src/script.js
+++ src/script.js
@@ -25,7 +25,7 @@ function formatDate(timestamp) {
 
 function displayForecast(response) {
   let forecast = response.data.daily;
-  let forecastElement = document.querySelector("#forecast")
+  let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
   console.log(forecastHTML);
