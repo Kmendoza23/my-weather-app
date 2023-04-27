59826f8c8b430fe443975aac572448d3e9139bd6
Kmendoza23 <k.mendozacontent@gmail.com>
2023-04-26 21:56:41 -0600

    changed API key for forecast

--- src/script.js
+++ src/script.js
@@ -55,12 +55,14 @@ function displayForecast(response) {
     });
 
   forecastHTML = forecastHTML + `</div>`;
+
   forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
 };
 
 
 function getForecast(coordinates) {
-  let apiKey = "0efb4fc16a9ed98dc0b3aafd8491d6ad";
+  let apiKey = "b40b135798f82a05aed08769f9275f50";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
